5aa699e62d4e79c19dba7f6af0c1682416698a0d
Ventura <ventura.py3@gmail.com>
2025-08-24 19:36:54 -0300

    chore: initial project setup

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+﻿module.exports = {
+    content: [
+        "./Pages/**/*.{html,js,razor,cshtml}",
+        "./Components/**/*.{html,js,razor,cshtml}",
+        "./Shared/**/*.{html,js,razor,cshtml}",
+        "./**/*.{html,js,razor,cshtml}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                primary: {
+                    DEFAULT: '#109CEB', // Blue
+                    light: '#3B82F6', // Light Blue
+                    dark: '#1E3A8A', // Dark Blue
+                },
+            },
+        },
+    },
+    plugins: [],
+}
